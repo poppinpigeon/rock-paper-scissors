ec3fabd8668e51b49bd4acdd7a48d676fdf2cdfe
Jade Kim <csia19030@gmail.com>
2024-10-07 23:49:30 +0900

    Create function to play a round

--- javascript.js
+++ javascript.js
@@ -1,1 +1,56 @@
-console.log("Hello World");
+function getComputerChoice(){
+    let rand = Math.random() * 8;
+    if(rand < 3){
+        return "rock";
+    }
+    else if(rand >= 3 && rand < 6){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+function getHumanChoice(){
+    let input = prompt("Rock, paper, or scissors?");
+    if(input.toLowerCase() === "rock"){
+        return "rock";
+    }
+    else if(input.toLowerCase() === "paper"){
+        return "paper";
+    }
+    else{
+        return "scissors"
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    console.log("Computer: " + computerChoice);
+    console.log("Human: " + humanChoice);
+
+    if(humanChoice === computerChoice){
+        console.log("It's a tie! Try again.");
+        return;
+    }
+    //human lose
+    else if((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")){
+        computerScore++;
+        console.log("You lose! " + computerChoice + " wins " + humanChoice + ".");
+        return;
+    }
+    //human win
+    else if((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")){
+        humanScore++;
+        console.log("You win! " + humanChoice + " wins " + computerChoice + ".");
+        return;
+    }
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+//test
+playRound(getHumanChoice(), getComputerChoice());
+console.log("humanscore: " + humanScore);
+console.log("computerscore: " + computerScore);
+
